8b300f1b1102251ad2ed9b865488ad726dd8b472
songzhikai <songzhikai@bonc.com.cn>
2018-07-13 10:05:17 +0800

    提交

---
+++ chapter10/eureka-server/src/main/java/kevin/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package kevin.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//此行注解代表为一个服务注册组件,此注解仅适用于eureka
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
